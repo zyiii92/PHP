734b68a451ee8666916be776a1518b848e852098
zyiii92 <125672164+zyiii92@users.noreply.github.com>
2024-02-29 15:30:56 +0800

    Add files via upload

---
+++ week01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>A1113316 0222 Homework</title>
+</head>
+<body>
+  <?php
+    echo "Have a nice day!";
+    echo "<br>";
+    echo "A1113316 莊依庭";
+    echo "<br>";
+  ?>
+</body>
+</html>

85891ccc4c074ddf7e5b2a63860adad872284ec1
zyiii92 <125672164+zyiii92@users.noreply.github.com>
2024-06-17 19:07:14 +0800

    Update index.php

--- week04/index.php
+++ week04/index.php
@@ -1,1 +1,80 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>資管晚會報名表單</title>
+        <link rel="stylesheet" href="index.css">  
+    </head>
 
+    <body>
+    
+    <div class="place0">
+        
+        <div class="word0">資管晚會報名表單</div>
+
+        <hr></hr>
+
+        <div class="word1">《資管之夜，與智慧共舞》</br></br></div>
+
+        <div class="word3">親愛的新生們，你們準備好了嗎？今年秋季，資訊管理系將為你帶來一場前所未有的晚會盛會！我們誠摯邀請你參加「資管之夜，與智慧共舞」晚會，這將是你大一生活中一個不可錯過的精彩時刻。</br></br>
+
+        在這個充滿活力和創意的晚上，你將有機會與同學們一起共享美好時光，認識更多志同道合的朋友。我們準備了豐富多彩的節目，包括現場表演、遊戲互動以及豐富的抽獎活動。無論你是熱愛音樂、舞蹈，還是喜歡挑戰遊戲的精英，這裡都會有你的舞台！</br></br>
+
+        除了娛樂，「資管之夜」還將為你提供一個絕佳的交流平台。你將有機會與系上的教授、學長姐以及業界人士近距離交流，了解資管專業的前沿趨勢和未來發展方向。這不僅是一場晚會，更是你職業生涯起點的一次寶貴體驗。</br></br>
+
+        所以，不要猶豫！快來加入我們，讓「資管之夜」成為你大學生活中的一個難忘片段。讓我們一起在智慧的光芒中共舞，創造屬於我們的精彩故事！</br></br>
+
+        更多詳情即將公佈，敬請期待！</br></br>
+
+        #資管之夜 #新生活動 #智慧共舞</br></br>
+
+        </div>
+
+        <hr></hr>
+
+        <div class="word2">地點：高大管院</div>
+        <div class="word2">時間：2028/11/31</div>
+
+        <hr></hr>
+
+        <div class="word1">活動內容</div>
+
+        <table>
+        <tr><th>時間</th><th>活動</th><th>備註</th></tr>
+        <tr><td>18:00~19:00</td><td>開場</td><td>報到</td></tr>
+        <tr><td>19:00~20:00</td><td>破冰</td><td></td></tr>
+        <tr><td>20:00~21:00</td><td>社團表演</td><td></td></tr>
+        <tr><td>:21:00~22:00</td><td>感性時間</td><td></td></tr>
+        <tr><td>22:00~23:00</td><td>團康</td><td></td></tr>
+        <tr><td>23:00~00:00</td><td>散場</td><td></td></tr>
+        </table>
+
+        <hr></hr>
+
+        <div class="word1">報名資料</div>
+
+        <div class="place1">
+        <div class="word4">
+        姓名: </div><input class="input1" type = "text" name = "sName" value = "" placeholder = "請輸入姓名" required></br>
+        </div>
+        <div class="place1">
+        <div class="word4">
+        學號: </div><input class="input1" type = "text" name = "sId" value = "" required></br>
+        </div>
+        <div class="place1">
+        <div class="word4">
+        電子郵件:</div> <input class="input1" type = "email" name = "sEmail" value = "" required></br>
+        </div>
+        <div class="place1">
+        <div class="word4">
+        參加意願: </div>
+        <input type = "radio" name = "sInterest" value = "要參加" checked>加一!
+        <input type = "radio" name = "sInterest" value = "不參加">這次先pass</br>
+        </div>
+        <div class="place1">
+        <a class="input2" href="form.php">報名</a></div>
+
+    </div>
+
+    </body>
+    </html>

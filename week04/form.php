812691c3d0b8fa883434394649bfe54e785fe2a1
zyiii92 <125672164+zyiii92@users.noreply.github.com>
2024-06-17 19:08:08 +0800

    Create form.php

---
+++ week04/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>資管晚會報名成功通知</title>
+        <link rel="stylesheet" href="index.css">  
+    </head>
+
+    <body>
+        
+        <div class="word0">報名成功!</div>
+
+    </body>
+    </html>
